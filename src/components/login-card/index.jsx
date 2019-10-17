499ff1cc5948ff200142e4ef18f70faea2dde674
martinmags <mmagsomb@ucsd.edu>
2019-10-17 09:54:05 -0700

    confusion with material-ui and css integration

--- src/components/login-card/index.jsx
+++ src/components/login-card/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import './style.scss';
 import {LongButton, SmallButton} from '../button/index.jsx';
 
+// React.Fragment tags?
 const LoginCard = (props) => {
   return (
     <div id="container">
-      <LongButton text="CONTINUE WITH FACEBOOK" />
-      <LongButton text="CONTINUE WITH GOOGLE" />
-      <h1> OR </h1>
-      <div id="names">
+      <React.Fragment>
+        <LongButton text="CONTINUE WITH FACEBOOK" />
+        <LongButton text="CONTINUE WITH GOOGLE" />
 
-      </div>
+        <Button variant="contained" className="mediaBtn">Continue With Facebook</Button>
+   
+        <h1> OR </h1>
+      </React.Fragment>
     </div>
   );
 }
