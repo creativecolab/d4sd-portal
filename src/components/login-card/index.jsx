abfd7ec32291df57921f88b8a7f07ab80c0495b3
Martin  Magsombol <bingsoo@martins-mbp.home>
2019-10-27 20:08:55 -0700

    login card component

--- src/components/login-card/index.jsx
+++ src/components/login-card/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Card, Input, Button } from 'antd';
 import './style.less';
+
 
 const LoginCard = (props) => {
     return (
-        <h1>cat</h1>
+      <Card id="card">
+        <p id="card-header">Login</p><br/>
+        <Input id="email-input" placeholder="Email" />        
+        <Input placeholder="Password" /><br/><br/>
+        <Button id="d4sd-btn" type="primary" block>Login</Button> 
+        <br/><br/>
+
+        <Button id="btn" type="primary" icon="google" block>Login with Google</Button>
+        <Button id="fb-btn" icon="facebook" block>Login with Facebook</Button>
+
+      </Card>
       );
 }
  
