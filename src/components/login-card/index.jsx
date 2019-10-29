1632c13ebc0c3980d3221a4369e007c494854400
StoneT2000 <stoneztao@gmail.com>
2019-10-29 09:51:33 -0700

    Revert "Integrated react-hook-forms"
    This reverts commit 975a743f2c3a608c9eb8674ea2f152e71a6e3c04.

--- src/components/login-card/index.jsx
+++ src/components/login-card/index.jsx
@@ -1,36 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import useForm from 'react-hook-form';
-import { Card, Input, Button, Form } from 'antd';
+import React from 'react';
+import { Card, Input, Button } from 'antd';
 import './style.less';
 
 
 const LoginCard = (props) => {
-    const { register, handleSubmit, setValue } = useForm();
-    const onSubmit = data => {
-      console.log(data);
-      // Add your axios stuff here
-    }
-
-    // handle changes and store to state with react hook forms
-    const handleChange = e => {
-      setValue(e.target.name, e.target.value);
-    };
-
-    // register inputs
-    useEffect(() => {
-       register({ name: "email-input" });
-       register({ name: "password" });
-    }, []);
-
-
     return (
       <Card id="card">
         <p id="card-header">Login</p><br/>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <Input id="email-input" placeholder="Email" name="email-input" onChange={handleChange}/>
-          <Input placeholder="Password" name="password" onChange={handleChange} /><br/><br/>
-          <Button id="d4sd-btn" type="primary" htmlType="submit" block>Login</Button>
-        </Form>
+        <Input id="email-input" placeholder="Email" />        
+        <Input placeholder="Password" /><br/><br/>
+        <Button id="d4sd-btn" type="primary" block>Login</Button> 
         <br/><br/>
 
         <Button id="btn" type="primary" icon="google" block>Login with Google</Button>
@@ -39,5 +18,5 @@ const LoginCard = (props) => {
       </Card>
       );
 }
-
+ 
 export default LoginCard;
