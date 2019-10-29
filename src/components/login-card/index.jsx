125c42e18d1639ca2032d19cf5f8f1505dfde205
Martin  Magsombol <bingsoo@wireless-169-228-116-44.ucsd.edu>
2019-10-29 09:50:12 -0700

    Changed BRs to margins; Changed width to 80vw and added a max-width

--- src/components/login-card/index.jsx
+++ src/components/login-card/index.jsx
@@ -7,9 +7,8 @@ const LoginCard = (props) => {
       <Card className="card-login">
         <p style={{fontSize: 22}}>Login</p><br/>
         <Input style={{marginBottom: 5}} placeholder="Email" />        
-        <Input placeholder="Password" /><br/><br/>
-        <Button className="d4sd-btn" block>Login</Button> 
-        <br/><br/>
+        <Input style={{marginBottom: 5}} placeholder="Password" />
+        <Button className="d4sd-btn" style={{marginBottom: 20}} block>Login</Button> 
         <Button className="google-btn" icon="google" block>Login with Google</Button>
         <Button className="fb-btn" icon="facebook" block>Login with Facebook</Button>
       </Card>
--- src/components/signup-card/index.jsx
+++ src/components/signup-card/index.jsx
@@ -5,10 +5,10 @@ import './style.less';
 const SignupCard = (props) => {
     return (
       <Card className="card-signup">
-        <p style={{fontSize: 22}}>Sign Up</p><br/>
+        <p style={{fontSize: 22, marginBottom: 10}}>Sign Up</p>
         <Button className="google-btn" icon="google" block>Continue with Google</Button>
-        <Button className="fb-btn" icon="facebook" block>Continue with Facebook</Button>
-        <br/>
+        <Button className="fb-btn" icon="facebook" style={{marginBottom: 10}} block>Continue with Facebook</Button>
+
         <p id="or-txt">OR</p>
         <Input.Group style={{marginTop: 5}}>
           <Row gutter={30} style={{marginBottom: 10}}>
@@ -19,11 +19,11 @@ const SignupCard = (props) => {
               <Input placeholder="Last Name" />
             </Col>
           </Row>
-          <Input style={{marginBottom: 5}} placeholder="Email" /><br/><br/>
-          <Input placeholder="Password" /><br/><br/>
+          <Input style={{marginBottom: 20}} placeholder="Email" />
+          <Input style={{marginBottom: 20}} placeholder="Password" />
         </Input.Group>
-        <Button className="d4sd-btn" type="primary" block>Sign Up</Button> 
-        <br/><br/>
+        <Button className="d4sd-btn" style={{marginBottom: 20}} block>Sign Up</Button> 
+
       </Card>
       );
 }
