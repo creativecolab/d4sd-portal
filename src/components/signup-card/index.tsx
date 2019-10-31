c4f647a1f04853777fae887abf0e01a241cdc9ca
StoneT2000 <stoneztao@gmail.com>
2019-10-30 23:14:28 -0700

    Error handling

--- src/components/signup-card/index.tsx
+++ src/components/signup-card/index.tsx
@@ -1,18 +1,25 @@
 import React, {useEffect} from 'react';
 import useForm from 'react-hook-form';
-import { Card, Input, Button, Col, Row, Form } from 'antd';
+import { Card, Input, Button, Col, Row, Form, message } from 'antd';
 import './style.less';
 
 const SignupCard = (props: any) => {
-  const { register, handleSubmit, setValue } = useForm();
+  const { register, handleSubmit, setValue, errors, triggerValidation, setError } = useForm();
   const onSubmit = (data: any) => {
+    /* Checks after attempting to submit */
+    if (!data.firstName) {
+      //setError("firstName", "notMatch", "Missing first name");
+    }
     console.log(data);
+    console.log(errors);
     // Add your axios stuff here
   }
 
   // handle changes and store to state with react hook forms
   const handleChange = (e: any) => {
-    setValue(e.target.name, e.target.value);
+    let name = e.target.name;
+    setValue(name, e.target.value);
+    console.log(errors);
   };
 
   // register inputs
@@ -24,16 +31,23 @@ const SignupCard = (props: any) => {
   }, []);
     return (
       <Card className="card-signup">
+        {errors.firstName && message.error(errors.firstName.message)}
+        {errors.lastName && message.error(errors.lastName.message)}
+        {errors.email && message.error(errors.email.message)}
+        {errors.password && message.error(errors.password.message)}
         <p className="signup-header">Sign Up</p>
         <Button className="google-btn" icon="google" block>Continue with Google</Button>
         <Button className="fb-btn" icon="facebook" block>Continue with Facebook</Button>
 
         <p className="or-txt">OR</p>
         <Form onSubmit={handleSubmit(onSubmit)}>
+
           <Input.Group className="input-group">
             <Row gutter={30} className="row-names">
               <Col span={12}>
+              //@ts-ignore
                 <Input placeholder="First Name" name='firstName' onChange={handleChange}/>
+
               </Col>
               <Col span={12}>
                 <Input placeholder="Last Name" name="lastName" onChange={handleChange}/>
