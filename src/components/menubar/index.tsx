872ce9f5c451780c8755276cb9eee82db21c74c1
StoneT2000 <stoneztao@gmail.com>
2019-10-31 16:45:49 -0700

    Matching Figma
    Just missing profile picture
    Also a little slow, not sure why

---
+++ src/components/menubar/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.less";
+import { Menu } from "antd";
+
+import logo from "../../assets/logo.svg";
+const { SubMenu } = Menu;
+
+function Menubar(props: any) {
+  return (
+    <Menu className="menubar" mode="horizontal">
+
+        <Menu.Item className="menu-item">
+          <img src={logo} alt="logo" />
+        </Menu.Item>
+        <div className="emptybar menu-item">
+        </div>
+        <Menu.Item className="menu-item"><span>Home</span></Menu.Item>
+        <Menu.Item className="menu-item"><span>About</span></Menu.Item>
+        <Menu.Item className="menu-item"><span>Resources</span></Menu.Item>
+        <Menu.Item className="menu-item"><span>Workspace</span></Menu.Item>
+
+    </Menu>
+  );
+};
+
+export default Menubar;
