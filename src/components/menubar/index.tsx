d5f2ab0b401fb2b0e3a20a3ace44f892e61041e7
StoneT2000 <stoneztao@gmail.com>
2019-10-31 19:47:56 -0700

    Smoother CSS

--- src/components/menubar/index.tsx
+++ src/components/menubar/index.tsx
@@ -10,16 +10,16 @@ function Menubar(props: any) {
   return (
     <Menu className="menubar" mode="horizontal">
 
-        <Menu.Item className="menu-item">
+        <Menu.Item className="menu-item-no">
           <img src={logo} alt="logo" />
         </Menu.Item>
-        <div className="emptybar menu-item">
+        <div className="emptybar menu-item-no">
         </div>
         <Menu.Item className="menu-item"><span>Home</span></Menu.Item>
         <Menu.Item className="menu-item"><span>About</span></Menu.Item>
         <Menu.Item className="menu-item"><span>Resources</span></Menu.Item>
         <Menu.Item className="menu-item"><span>Workspace</span></Menu.Item>
-        <Menu.Item className="menu-profile"><img src={logo} alt="avatar" /></Menu.Item>
+        <Menu.Item className="menu-profile menu-item-no"><img src={logo} alt="avatar" /></Menu.Item>
 
     </Menu>
   );
