9637862bbe803708403c13dd6ca75e9aa4678e94
leestanley <stanleylee91776@gmail.com>
2019-11-01 17:08:36 -0700

    nice it finally works

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  extends: ["airbnb-typescript"],
+  extends: ["airbnb-base", "plugin:@typescript-eslint/recommended"],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
