9fec3e836a8a3dc270bbd6cc456feec3780a92af
StoneT2000 <stoneztao@gmail.com>
2019-11-01 21:56:54 -0700

    Fix x-overflow bug
    Ant has a bug where the row with gutter specified may overflow its container due to negative margins

--- src/components/layouts/signup-layout/index.tsx
+++ src/components/layouts/signup-layout/index.tsx
@@ -5,17 +5,17 @@ import './style.less'
 
 const SignupLayout =(props: any)=>{
   return(
-    <div>
+    <div className="SignupLayout">
       <Row type="flex" justify="center" align="middle" gutter={16}>
         <Col span={10}>
           <h1>
-              Welcome to 
+              Welcome to
               D4SD's Workspace
           </h1>
           <p style={{marginTop: "40px"}}>
-            Create teams, submit design solutions, 
-            and receive feedback on your work. 
-            Sign up for the workspace today. 
+            Create teams, submit design solutions,
+            and receive feedback on your work.
+            Sign up for the workspace today.
           </p>
         </Col>
         <Col offset={2} span={8}>
