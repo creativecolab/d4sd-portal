9d1c4d2c4ebc294c9d7eeccaa7beceaa0a06b596
Martin  Magsombol <bingsoo@martins-mbp.dynamic.ucsd.edu>
2019-11-05 11:42:09 -0800

    sign-up button successfully writes user to firestore

--- src/actions/firebase.ts
+++ src/actions/firebase.ts
@@ -3,28 +3,16 @@ import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_API_KEY,
-  databaseURL: process.env.REACT_APP_API_KEY,
-  projectId: process.env.REACT_APP_API_KEY,
-  storageBucket: process.env.REACT_APP_API_KEY,
-  messagingSenderId: process.env.REACT_APP_API_KEY,
-  appId: process.env.REACT_APP_API_KEY,
-  measurementId: process.env.REAsCT_APP_API_KEY
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
-/**
- * Verify Firebase
- */
-
- 
 const db = firebase.firestore();
-// const snapshot = await db
-//   .collection("users")
-//   .orderBy("lastModified")
-//   .limit(100)
-//   .get();
-
-// const docRef = db.doc('users');
 
 export const registerUser = (data: any) =>{
   // Add a new user w/ a generated id to "users" collection.
@@ -39,9 +27,8 @@ export const registerUser = (data: any) =>{
   }).catch(error => {
     console.log("Error writing document: ", error);
   });
-
-  console.log(newUser);
 }
+
 export const loginUser = () => {}
 export const logoutUser = () => {}
 
