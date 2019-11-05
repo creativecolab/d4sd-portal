7f5770f1e79854b0ac1799aa33a559cd3a3ff713
StoneT2000 <stoneztao@gmail.com>
2019-11-05 00:04:03 -0800

    Finishing up menu bar

--- src/components/menubar/index.tsx
+++ src/components/menubar/index.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./style.less";
 import { Menu } from "antd";
 
-import logo from "../../assets/logo.svg";
+import d4sdlogo from "../../assets/logo.svg";
+import d4sdlogoBlue from "../../assets/logo-blue.svg";
+import daniel from "../../assets/daniel.png";
 const { SubMenu } = Menu;
 
 function Menubar(props: any) {
-
+  let [scroll, setScroll] = useState("");
+  let [logo, setLogo] = useState(d4sdlogoBlue);
+  useEffect(() => {
+    if (scroll == "") {
+      logo = d4sdlogo;
+    }
+    else {
+      logo = d4sdlogoBlue;
+    }
+  }, [scroll]);
   return (
-    <Menu className="menubar" mode="horizontal">
+    <Menu id="menubar" mode="horizontal" className={scroll}>
 
-        <Menu.Item className="menu-item-no">
+        <Menu.Item className="menu-item-no menu-logo">
           <img src={logo} alt="logo" />
         </Menu.Item>
         <div className="emptybar menu-item-no">
@@ -19,7 +30,7 @@ function Menubar(props: any) {
         <Menu.Item className="menu-item"><span>About</span></Menu.Item>
         <Menu.Item className="menu-item"><span>Resources</span></Menu.Item>
         <Menu.Item className="menu-item"><span>Workspace</span></Menu.Item>
-        <Menu.Item className="menu-profile menu-item-no"><img src={logo} alt="avatar" /></Menu.Item>
+        <Menu.Item className="menu-profile menu-item-no"><img src={daniel} alt="avatar" /></Menu.Item>
 
     </Menu>
   );
