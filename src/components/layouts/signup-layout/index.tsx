4735155eb455b9cdca06725e1b5543cb2c2c1a84
Paridhi Khaitan <44253653+paridhikhaitan@users.noreply.github.com>
2019-11-14 14:43:21 -0800

    Switch cards (#68)
    * Added step timeline and hooks that can be used to switch
    
    - No actual switching yet.
    
    * Package update and some minor CSS
    
    * Switched cards for signup flow

--- src/components/layouts/signup-layout/index.tsx
+++ src/components/layouts/signup-layout/index.tsx
@@ -1,37 +1,78 @@
 import React, { useState } from 'react';
 import SignupCard from '../../signup-card/index';
-import {Row, Col} from '@d4sd/components';
+import {Row, Col, Steps, Button} from '@d4sd/components';
 import Menubar from '../../menubar/index';
 import './style.less'
 import LoginCard from '../../login-card';
 import RoleCard from '../../role-card';
 import EthicsCard from '../../ethics-card';
 
-const SignupLayout =(props: any)=>{
-  const [showEthics, setShowEthics]= useState(false);
-  const [showRole, setShowRole]= useState(false);
+const {Step} = Steps;
 
+const SignupLayout =(props: any)=>{
+
+  // hook, must be one of "start, role, ethics, etc."
+  const [signupStep, setSignupStep] = useState("start");
+  const mapSignupStepToNum = function(signupStep: String) {
+    switch (signupStep) {
+      case "start":
+        return 0;
+      case "role":
+        return 2;
+      case "ethics":
+        return 3;
+      case "success":
+        return 4;
+    default :
+        return 0;
+    }
+  }
+
+
+  // signupStep
   return(
     <div>
       <Menubar />
       <div className="SignupLayout">
-        <Row type="flex" justify="center" align="middle" gutter={[16, 48]}>
-          <Col md={{span:10}} xs={{span: 20}}>
-            <h1 id='welcome-message'>
-                Welcome to
-                D4SD's Workspace
-            </h1>
-            <p className="subText">
-              Create teams, submit design solutions,
-              and receive feedback on your work.
-              Sign up for the workspace today.
-            </p>
-          </Col>
-          <Col md={{offset: 2, span: 8}} sm={{span: 16}} xs={{span: 20}} >
-            <SignupCard />
-            {showEthics && <EthicsCard />}
-          </Col>
-        </Row>
+      <Row className="heading">
+      <h1 className='heading-message'>
+        {signupStep === "start" && "Workspace Sign Up"}
+        {signupStep === "role" && "Choose Your Role"}
+        {signupStep === "ethics" && "Code of Ethics"}
+        {signupStep === "success" && "Welcome to D4SD Workspace!"}
+        {signupStep === "login" && "D4SD Workspace"}
+      </h1>
+      </Row>
+      <Row className="card-container">
+
+        {signupStep === "start" && <SignupCard />}
+        {signupStep != "start" && signupStep != "login" &&
+        <Steps size="small" current={mapSignupStepToNum(signupStep)} className="step-flow">
+          <Step title="Sign Up" />
+          <Step title="Email Confirmation" />
+          <Step title="Choose Your Role" key='role'/>
+          <Step title="Code of Ethics" key='ethics'/>
+          <Step title="Success " key='success'/>
+        </Steps>}
+        {signupStep === "role" && <RoleCard setSignupStep={setSignupStep}/>}
+        {signupStep === "ethics" && <EthicsCard setSignupStep={setSignupStep}/>}
+        {signupStep === "success" && <div>
+          <Row type="flex" justify="center">
+            <Col span="20">
+              <h2>You’ve successfully joined the D4SD 2020 Challenge! 
+                Go to your Workspace to register your team.</h2>
+            </Col>
+          </Row>
+          <Row type="flex" justify="center">
+            <Col span={10}>
+              <Button onClick={()=> setSignupStep("login")}>
+                GO TO WORKSPACE
+              </Button>
+            </Col>
+          </Row>
+        </div>}
+        {signupStep === "login" && <LoginCard />}
+      </Row>
       </div>
     </div>
   );
