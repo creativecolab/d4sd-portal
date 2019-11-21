c85f89d806f68ff01e67dbf5ee38114c1406f3e6
Stanley Lee <stanleylee91776@gmail.com>
2019-11-21 09:58:59 -0800

    added eslint fixes (#81)

--- src/components/role-card/index.tsx
+++ src/components/role-card/index.tsx
@@ -1,45 +1,45 @@
 import React from 'react';
-import {Radio, Button} from '@d4sd/components';
+import { Radio, Button } from '@d4sd/components';
 import './style.less';
 
 import { render } from 'react-dom';
 
-const RoleCard= (props: any) => {
-  return(
-    <div className="role-card">
-      <Radio.Group className="choices">
-          <div className="choice">
-            <Radio value="participant" /><label>
-              <h4 className="subHeading">Sign up as a Participant</h4>
-              <br />
-              <p>
+const RoleCard = (props: any) => (
+  <div className="role-card">
+    <Radio.Group className="choices">
+      <div className="choice">
+        <Radio value="participant" />
+        <label>
+          <h4 className="subHeading">Sign up as a Participant</h4>
+          <br />
+          <p>
               As a participant, you are
               able to meet new people and
               tackle SD’s problems with a team
               of like-minded individuals.
-              </p>
+          </p>
 
-            </label>
-          </div>
-          <div className="choice">
-            <Radio value="feedback" /><label>
-              <h4 className="subHeading">Sign up as a Feedback Provider</h4>
-              <br />
-              <p>
+        </label>
+      </div>
+      <div className="choice">
+        <Radio value="feedback" />
+        <label>
+          <h4 className="subHeading">Sign up as a Feedback Provider</h4>
+          <br />
+          <p>
               As a feedback provider, you are able to
               give teams valuable input for their design
               projects after feedback window opens.
-              </p>
-            </label>
-          </div>
-        </Radio.Group>
-        <div className="bottom-btn">
-          <Button className="d4sd-btn" block type="primary" size="small" htmlType="submit" onClick={()=> props.setSignupStep("ethics") }>
+          </p>
+        </label>
+      </div>
+    </Radio.Group>
+    <div className="bottom-btn">
+      <Button className="d4sd-btn" block type="primary" size="small" htmlType="submit" onClick={() => props.setSignupStep('ethics')}>
             CONTINUE
-          </Button>
-        </div>
+      </Button>
     </div>
-  );
-};
+  </div>
+);
 
 export default RoleCard;
