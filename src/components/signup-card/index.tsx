3dcbf9431aeff2dd89b38935d3b51bcfa5bbc75c
Martin Magsombol <mmagsomb@ucsd.edu>
2019-12-05 16:26:19 -0800

    o nothing really

--- src/components/signup-card/index.tsx
+++ src/components/signup-card/index.tsx
@@ -1,40 +1,38 @@
-import React, { useEffect } from 'react';
-import useForm from 'react-hook-form';
-import './style.less';
-import {
-  Card, Input, Button, Col, Row, Form, message,
-} from '@d4sd/components';
+import React, { useEffect } from "react";
+import useForm from "react-hook-form";
+import "./style.less";
+import { Card, Input, Button, Col, Row, Form, message } from "@d4sd/components";
 
 const emailRegExp = new RegExp(
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 );
 
 const SignupCard = (props: any) => {
-  const {
-    register, handleSubmit, setValue, errors,
-  } = useForm();
+  const { register, handleSubmit, setValue, errors } = useForm();
   const onSubmit = (data: any) => {
     /* Checks after attempting to submit */
     if (!data.firstName) {
-      message.error('Missing first name');
+      message.error("Missing first name");
       return;
     }
     if (!data.lastName) {
-      message.error('Missing last name');
+      message.error("Missing last name");
       return;
     }
     if (!data.email) {
-      message.error('Missing email');
+      message.error("Missing email");
       return;
-    } if (!emailRegExp.test(data.email)) {
-      message.error('Please input a valid email');
+    }
+    if (!emailRegExp.test(data.email)) {
+      message.error("Please input a valid email");
       return;
     }
     if (!data.password) {
-      message.error('Missing password');
+      message.error("Missing password");
       return;
-    } if (data.password.length < 6) {
-      message.error('Password must be 6 charaters long');
+    }
+    if (data.password.length < 6) {
+      message.error("Password must be 6 charaters long");
       return;
     }
     console.log(data);
@@ -50,10 +48,10 @@ const SignupCard = (props: any) => {
 
   // register inputs
   useEffect(() => {
-    register({ name: 'email' });
-    register({ name: 'password' });
-    register({ name: 'firstName' });
-    register({ name: 'lastName' });
+    register({ name: "email" });
+    register({ name: "password" });
+    register({ name: "firstName" });
+    register({ name: "lastName" });
   }, []);
   return (
     <div className="card-signup-wrapper">
@@ -98,15 +96,20 @@ const SignupCard = (props: any) => {
           </Input.Group>
           <Row type="flex" justify="center">
             <a href="/signup/role" className="continue-btn">
-              <Button className="d4sd-btn" type="primary" htmlType="submit" onClick={() => props.setSignupStep('role')}>SIGN UP</Button>
+              <Button
+                className="d4sd-btn"
+                type="primary"
+                htmlType="submit"
+                onClick={() => props.setSignupStep("role")}
+              >
+                SIGN UP
+              </Button>
             </a>
           </Row>
         </Form>
       </div>
       <p id="bottom-txt">
-        Already have an account?
-        {' '}
-        <a id="login-link">Log in</a>
+        Already have an account? <a id="login-link">Log in</a>
       </p>
     </div>
   );
