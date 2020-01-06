94812fc1a4e8002afdb943fffc84dbd427fd3b6b
Stone Tao <stonezt2019@gmail.com>
2020-01-06 13:39:47 -0800

    Preliminary submission (#93)
    * Initial
    
    * More pages
    
    * Upload stuff
    
    * Save changes in between
    
    * d4sd components update
    
    * fix warnings in useEffect
    
    * save teamname to localstorage
    
    * save prelim step to localstorage
    
    * add react-app-polyfill
    
    Co-authored-by: Lee Liang <51972171+lliang19@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,13 @@ module.exports = {
       typescript: {}
     }
   },
+  env: {
+    browser: true,
+    node: true
+  },
   rules: {
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-    'react/jsx-max-props-per-line': [2, { maximum: 3, when: 'always'}],
+    'react/jsx-max-props-per-line': [2, { maximum: 3, when: 'always' }],
     'comma-dangle': [2, 'never'],
     'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
     '@typescript-eslint/indent': [2, 2]
