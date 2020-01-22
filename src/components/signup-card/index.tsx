e06f5501a95f91f4c4fac822489db6d9c650d292
leestanley <stanleylee91776@gmail.com>
2020-01-22 13:40:57 -0800

    regex

--- src/components/signup-card/index.tsx
+++ src/components/signup-card/index.tsx
@@ -14,6 +14,8 @@ const emailRegExp = new RegExp(
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 );
 
+const passRegExp = new RegExp(/^((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})$/);
+
 // eslint-disable-next-line
 const SignupCard = (props: any): JSX.Element => {
   const { setSignupStep } = props;
@@ -42,8 +44,8 @@ const SignupCard = (props: any): JSX.Element => {
     if (!data.password) {
       message.error('Missing password');
       return false;
-    } if (data.password.length < 6) {
-      message.error('Password must be 6 charaters long');
+    } if (!passRegExp.test(data.password)) {
+      message.error('Password must be 6 charaters, one uppercase, and one lowercase character');
       return false;
     }
     return true;
@@ -129,6 +131,7 @@ const SignupCard = (props: any): JSX.Element => {
               className="input-email"
               placeholder="Email"
               name="email"
+              type="email"
               onChange={handleChange}
             />
             <Input.Password
