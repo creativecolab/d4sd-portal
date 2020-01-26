afd8612bfbbda5271d2a5c567fade00965cd755a
Paridhi Khaitan <paridhi4699@gmail.com>
2020-01-26 13:55:15 -0800

    added problem statement removed pdf

--- src/components/layouts/feedback-provider-layout/index.tsx
+++ src/components/layouts/feedback-provider-layout/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Menubar from '../../menubar';
-import {Row, Col, Form, Input} from '@d4sd/components'
+import {Row, Col, Form, Input, Button} from '@d4sd/components'
 import ProviderInformation from './provider-information'
 import QuestionList from './question-list'
 import SubmissionPreview from './submission-preview';
+import Header from '../../Header/index';
 
 const FeedbackProviderLayout = (props: any) => {
 
+  const onSubmit = (data: any) => {
+    console.log(data);
+    //Write to firebase
+  }
+
   return(
     <div>
-
-      <Row type="flex" justify="center">
+      <div className="header">
+      <Row type="flex" justify="center" className="subtxt">
         <Col span={18}>
           <h1>Provide Feedback</h1>
           <p> Welcome and thank you for agreeing to provide feedback! 
@@ -22,9 +27,10 @@ const FeedbackProviderLayout = (props: any) => {
         </Col>
      
       </Row>
+      </div>
       <ProviderInformation />
 
-      <Row>
+      <Row className="provideInput"> 
         <h1>Review and Provide Input</h1>
       </Row>
 
@@ -34,6 +40,12 @@ const FeedbackProviderLayout = (props: any) => {
         </Col>
         <Col xs={{span: 20}} md={{span: 10}}>
           <QuestionList />
+        </Col>
+      </Row>
+
+      <Row type= "flex" justify="center" className="submit-btn-feedback">
+        <Col>
+        <Button htmlType="submit" type="primary">SUBMIT</Button>
         </Col>
       </Row>
     </div> 
