7cb45aff33f68660019e23779e3c0249bf75555c
Paridhi Khaitan <paridhi4699@gmail.com>
2020-01-31 23:43:41 -0800

    Submission display page done

--- src/components/preliminarysubmission-cards/feedback/index.tsx
+++ src/components/preliminarysubmission-cards/feedback/index.tsx
@@ -3,14 +3,50 @@ import useForm from "react-hook-form";
 import { Form, Row, Col } from "@d4sd/components";
 import "./style.less";
 import DOMPurify from "dompurify";
-import { Input } from "antd";
+import { Input, Button } from "@d4sd/components";
 import TextArea from "antd/lib/input/TextArea";
+import PDFPreview from "./pdf-preview";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#FFFFFF"
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
 
 interface FeedbackCardIF {
   setSubmitStep(step: string): void;
 }
 
 const FeedbackCard = (props: FeedbackCardIF): JSX.Element => {
+  // Create Document Component
+  const MyDocument = () => (
+
+    //Document has an onRender prop that can receive the Blob from Web
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </View>
+      </Page>
+    </Document>
+  );
+
   const { setSubmitStep } = props;
   const { register, handleSubmit, setValue } = useForm();
   const projectName = localStorage.getItem("teamName-d4sd-prelim-submit");
@@ -18,13 +54,17 @@ const FeedbackCard = (props: FeedbackCardIF): JSX.Element => {
     "problemstatement-d4sd-prelim-submit"
   );
 
-  const prelimQuestion1 = localStorage.getItem("prelim-submission-question1") || "";
-  const prelimQuestion2 = localStorage.getItem("prelim-submission-question2") || "";
-  const prelimQuestion3 = localStorage.getItem("prelim-submission-question3") || "";
-
+  const prelimQuestion1 =
+    localStorage.getItem("prelim-submission-question1") || "";
+  const prelimQuestion2 =
+    localStorage.getItem("prelim-submission-question2") || "";
+  const prelimQuestion3 =
+    localStorage.getItem("prelim-submission-question3") || "";
   const questionList =
     "<p>Here’s your opportunity to get input on your ideas. Your team can pose up to three questions to ask the community about your proposed solutions. Asking questions is optional but can really help facilitate feedback exchange. Advice for writing effective questions:</p>";
 
+  const expertiseRequired =
+    localStorage.getItem("prelim-submission-expertise") || "";
   /* eslint-disable */
   const onSubmit = (data: any): void => {
     console.log(data);
@@ -63,13 +103,15 @@ const FeedbackCard = (props: FeedbackCardIF): JSX.Element => {
               <p>
                 <b>1. Problem Statement</b>
               </p>
-              <div>
+              <div className="problem-statement">
                 <p>{problemStatement}</p>
               </div>
               <p>
                 <b>2. Propose Initial Concepts</b>
-                <div>Show PDF</div>
               </p>
+              <div className="pdf-display">
+                <MyDocument />
+              </div>
             </Row>
           </Col>
           <Col sm={{ span: 2 }}></Col>
@@ -110,21 +152,72 @@ const FeedbackCard = (props: FeedbackCardIF): JSX.Element => {
             </ul>
 
             <Form layout="vertical" onSubmit={handleSubmit(onSubmit)}>
-              <Input.Group>
+              <div>
                 <h4 className="questions">Question 1</h4>
-                <TextArea autoSize={{minRows: 3, maxRows: 5}} onChange={handleChange} name="prelim-submission-question1">
-                </TextArea>
-                <br />
+                <TextArea
+                  autoSize={{ minRows: 4, maxRows: 5 }}
+                  onChange={handleChange}
+                  name="prelim-submission-question1"
+                  value={prelimQuestion2}
+                ></TextArea>
+              </div>
+              <br />
+              <div>
                 <h4 className="questions">Question 2</h4>
-                <TextArea autoSize={{minRows: 3, maxRows: 5}} onChange={handleChange} name="prelim-submission-question2" value={prelimQuestion2}>
-                </TextArea>
-
+                <TextArea
+                  autoSize={{ minRows: 4, maxRows: 5 }}
+                  onChange={handleChange}
+                  name="prelim-submission-question2"
+                  value={prelimQuestion2}
+                ></TextArea>
+              </div>
+              <br />
+              <div>
                 <h4 className="questions">Question 3</h4>
-                <TextArea autoSize={{minRows: 3, maxRows: 5}} onChange={handleChange} name="prelim-submission-question3" value={prelimQuestion3}>
-                </TextArea>
-              </Input.Group>
+                <TextArea
+                  autoSize={{ minRows: 4, maxRows: 5 }}
+                  onChange={handleChange}
+                  name="prelim-submission-question3"
+                  value={prelimQuestion3}
+                ></TextArea>
+              </div>
+              <br />
+              <div>
+                <p>
+                  D4SD will try to recruit feedback providers from our community
+                  network. What areas of expertise are you looking for?
+                </p>
+                <TextArea
+                  autoSize={{ minRows: 4, maxRows: 5 }}
+                  onChange={handleChange}
+                  name="prelim-submission-expertise"
+                  value={expertiseRequired}
+                ></TextArea>
+              </div>
             </Form>
           </Col>
+        </Row>
+
+        <Row type="flex" justify="center" className="bottom-btns">
+          <Button
+            type="primary"
+            onClick={(): void => {
+              console.log("back");
+            }}
+          >
+            BACK
+          </Button>
+          <Button type="primary-outline" size="large">
+            SAVE WORK
+          </Button>
+          <Button
+            type="primary"
+            onClick={(): void => {
+              setSubmitStep("done");
+            }}
+          >
+            NEXT
+          </Button>
         </Row>
       </div>
     </div>
