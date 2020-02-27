2b46e1261efb51b2f0a8370b6de2aff391599290
StoneT2000 <stoneztao@gmail.com>
2020-02-27 11:23:23 -0800

    New buttons plus disabled

--- src/components/layouts/attend-layout/index.tsx
+++ src/components/layouts/attend-layout/index.tsx
@@ -66,7 +66,7 @@ const AttendLayout = (): JSX.Element => {
           <p>{attendContent.content_1_1}</p>
           <p>{attendContent.content_1_2}</p>
           <br />
-          <Button>REGISTER ON EVENTBRITE</Button>
+          <Button type="primary" disabled>REGISTER ON EVENTBRITE</Button>
         </div>
       </div>
       {/* <div className="event-details">
