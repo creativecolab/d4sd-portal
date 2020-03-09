4f88ec6489ac52ee095316f9215670a9212b320e
Lee Liang <ltliang1@gmail.com>
2020-03-09 14:38:32 -0700

    implement route based splitting w/ react-loadable

--- src/index.tsx
+++ src/index.tsx
@@ -7,29 +7,112 @@ import { ConnectedRouter } from 'connected-react-router';
 import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReactGA from 'react-ga';
+import Loadable from 'react-loadable';
 import configureStore, { history } from './store';
 
-import ScrollToTop from './components/ScrollToTop';
-import SignupPage from './containers/signup-page';
-import LoginPage from './containers/login-page';
-import HomePage from './containers/home-page';
-import InvolvedPage from './containers/involved-page';
-import AboutPage from './containers/about-page';
-import ChallengePage from './containers/challenge-page';
-import AttendPage from './containers/attend-page';
-import ResourcesPage from './containers/resources-page';
-import FAQPage from './containers/faq-page';
-import SponsorsPage from './containers/sponsors-page';
-import WorkspacePage from './containers/workspace-page';
-import RequestFeedbackPage from './containers/request-feedback-page';
-import SubmitProposalPage from './containers/submit-proposal-page';
-import ProcessPage from './components/layouts/process-layout';
-import StakeholderPage from './components/layouts/stakeholder-layout';
-import FeedbackPage from './components/layouts/feedback-layout';
-import ViewFeedbackLayout from './components/layouts/view-feedback-layout';
-import CommunityFeedbackLayout from './components/layouts/community-feedback-layout';
-import PreliminarySubmissionPage from './components/layouts/preliminary-submission-layout';
+// import ScrollToTop from './components/ScrollToTop';
+// import SignupPage from './containers/signup-page';
+// import LoginPage from './containers/login-page';
+// import HomePage from './containers/home-page';
+// import InvolvedPage from './containers/involved-page';
+// import AboutPage from './containers/about-page';
+// import ChallengePage from './containers/challenge-page';
+// import AttendPage from './containers/attend-page';
+// import ResourcesPage from './containers/resources-page';
+// import FAQPage from './containers/faq-page';
+// import SponsorsPage from './containers/sponsors-page';
+// import WorkspacePage from './containers/workspace-page';
+// import RequestFeedbackPage from './containers/request-feedback-page';
+// import SubmitProposalPage from './containers/submit-proposal-page';
+// import ProcessPage from './components/layouts/process-layout';
+// import StakeholderPage from './components/layouts/stakeholder-layout';
+// import FeedbackPage from './components/layouts/feedback-layout';
+// import ViewFeedbackLayout from './components/layouts/view-feedback-layout';
+// import CommunityFeedbackLayout from './components/layouts/community-feedback-layout';
+// import PreliminarySubmissionPage from './components/layouts/preliminary-submission-layout';
 // import FeedbackProviderLayout from './components/layouts/feedback-provider-layout';
+
+const Loading = (): null => null; // loading component
+const ScrollToTop = Loadable({
+  loader: () => import('./components/ScrollToTop'),
+  loading: Loading
+});
+const HomePage = Loadable({
+  loader: () => import('./containers/home-page'),
+  loading: Loading
+});
+const SignupPage = Loadable({
+  loader: () => import('./containers/signup-page'),
+  loading: Loading
+});
+const LoginPage = Loadable({
+  loader: () => import('./containers/login-page'),
+  loading: Loading
+});
+const InvolvedPage = Loadable({
+  loader: () => import('./containers/involved-page'),
+  loading: Loading
+});
+const AboutPage = Loadable({
+  loader: () => import('./containers/about-page'),
+  loading: Loading
+});
+const ChallengePage = Loadable({
+  loader: () => import('./containers/challenge-page'),
+  loading: Loading
+});
+const AttendPage = Loadable({
+  loader: () => import('./containers/attend-page'),
+  loading: Loading
+});
+const ResourcesPage = Loadable({
+  loader: () => import('./containers/resources-page'),
+  loading: Loading
+});
+const FAQPage = Loadable({
+  loader: () => import('./containers/faq-page'),
+  loading: Loading
+});
+const SponsorsPage = Loadable({
+  loader: () => import('./containers/sponsors-page'),
+  loading: Loading
+});
+const SubmitPage = Loadable({
+  loader: () => import('./containers/workspace-page'),
+  loading: Loading
+});
+const RequestFeedbackPage = Loadable({
+  loader: () => import('./containers/request-feedback-page'),
+  loading: Loading
+});
+const SubmitProposalPage = Loadable({
+  loader: () => import('./containers/submit-proposal-page'),
+  loading: Loading
+});
+const ProcessPage = Loadable({
+  loader: () => import('./components/layouts/process-layout'),
+  loading: Loading
+});
+const StakeholderPage = Loadable({
+  loader: () => import('./components/layouts/stakeholder-layout'),
+  loading: Loading
+});
+const FeedbackPage = Loadable({
+  loader: () => import('./components/layouts/feedback-layout'),
+  loading: Loading
+});
+// const ViewFeedbackLayout = Loadable({
+//   loader: () => import('./components/layouts/view-feedback-layout'),
+//   loading: Loading
+// });
+// const CommunityFeedbackLayout = Loadable({
+//   loader: () => import('./components/layouts/community-feedback-layout'),
+//   loading: Loading
+// });
+// const PreliminarySubmissionPage = Loadable({
+//   loader: () => import('./components/layouts/preliminary-submission-layout'),
+//   loading: Loading
+// });
 
 // eslint-disable-next-line
 // @ts-ignore
@@ -73,12 +156,6 @@ const App = (): JSX.Element => {
               <Route exact path="/about" component={AboutPage} />
               <Route exact path="/sponsor" component={SponsorsPage} />
               <Route exact path="/faq" component={FAQPage} />
-              <Route exact path="/workspace" component={WorkspacePage} />
-              <Route
-                exact
-                path="/workspace/community-feedback"
-                component={CommunityFeedbackLayout}
-              />
               <Route exact path="/resources" component={ResourcesPage} />
               <Route exact path="/resources/process" component={ProcessPage} />
               <Route
@@ -86,9 +163,10 @@ const App = (): JSX.Element => {
                 path="/resources/stakeholder"
                 component={StakeholderPage}
               />
-              <Route exact path="/submit" component={WorkspacePage} />
+              <Route exact path="/submit" component={SubmitPage} />
               <Route exact path="/submit/request-feedback" component={RequestFeedbackPage} />
               <Route exact path="/submit/submit-proposal" component={SubmitProposalPage} />
+              {/*
               <Route
                 exact
                 path="/workspace/prelim"
@@ -99,7 +177,11 @@ const App = (): JSX.Element => {
                 path="/workspace/view-feedback"
                 component={ViewFeedbackLayout}
               />
-              {/*
+              <Route
+                exact
+                path="/workspace/community-feedback"
+                component={CommunityFeedbackLayout}
+              />
               <Route exact path='/resources' component={Resources}/>
               <Route exact path='/workspace' component={Workspace}/>
 
