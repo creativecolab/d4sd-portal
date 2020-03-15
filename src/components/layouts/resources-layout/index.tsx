2cb00c037b434c4004372b944050c90241c960f1
leestanley <stanleylee91776@gmail.com>
2020-03-15 01:24:45 -0700

    hotfixes

--- src/components/layouts/resources-layout/index.tsx
+++ src/components/layouts/resources-layout/index.tsx
@@ -95,157 +95,26 @@ function ResourcesLayout() {
           </div>
         </div>
       </div>
-      {/* <Tabs>
-        <TabDiv label="Process">
-          <div className="container paragraph">
-            <div className="red">
-              <h1 className="cardtitle">FEATURED RESOURCES</h1>
-              <Row type="flex" justify="center" gutter={[32, 16]}>
-                <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-                  <NavLink to="/resources/process">
-                    <Card className="involve-card">
-                      <h1 className="title">D4SD Process Guide</h1>
-                      <h3 className="source">Design for San Diego</h3>
-                      <p>
-                        Modeled after best practices in human-centered design
-                        research, the D4SD process involves five key phases:
-                        Discovery, Ideate, Prototype, Pitch, Activate. Read more
-                        about how you and your team can get the most out of each
-                        phase.
-                      </p>
-                    </Card>
-                  </NavLink>
-                </Col>
-                {resourcesContent.process.featured.map(content => (
-                  <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-                    <a href={content.link} target="_blank">
-                      <Card className="involve-card">
-                        <h1 className="title">{content.title}</h1>
-                        <h3 className="source">{content.source}</h3>
-                        <p>{content.description}</p>
-                      </Card>
-                    </a>
-                  </Col>
-                ))}
-              </Row>
-              <h1 className="othertitle">OTHER RESOURCES</h1>
-              {resourcesContent.process.other.map(content => (
-                <a href={content.link} target="_blank">
-                  <div className="extra">
-                    <h1>{content.title}</h1>
-                    <h3>{content.source}</h3>
-                  </div>
-                </a>
-              ))}
-            </div>
-          </div>
-        </TabDiv>
-        <TabDiv label="Community">
-          <div className="container paragraph">
-            <div className="blue">
-              <h1 className="cardtitle">FEATURED RESOURCES</h1>
-              <Row type="flex" justify="center" gutter={[32, 16]}>
-                <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-                  <NavLink to="/resources/stakeholder">
-                    <Card className="involve-card">
-                      <h1 className="title">Stakeholder Relationships</h1>
-                      <h3 className="source">Design for San Diego</h3>
-                      <p>
-                        Good civic design involves understanding the diverse
-                        perspectives of many stakeholders, including people
-                        directly and indirectly impacted by a problem or
-                        opportunity. This guide can help D4SD participants
-                        establish and cultivate community relationships.
-                      </p>
-                    </Card>
-                  </NavLink>
-                </Col>
-                {resourcesContent.community.featured.map(content => (
-                  <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-                    <a href={content.link} target="_blank">
-                      <Card className="involve-card">
-                        <h1 className="title">{content.title}</h1>
-                        <h3 className="source">{content.source}</h3>
-                        <p>{content.description}</p>
-                      </Card>
-                    </a>
-                  </Col>
-                ))}
-              </Row>
-              <h1 className="othertitle">OTHER RESOURCES</h1>
-              {resourcesContent.community.other.map(content => (
-                <a href={content.link} target="_blank">
-                  <div className="extra-community">
-                    <h1>{content.title}</h1>
-                    <h3>{content.source}</h3>
-                  </div>
-                </a>
-              ))}
-            </div>
-          </div>
-        </TabDiv>
-        <TabDiv label="Data">
-          <div className="container paragraph">
-            <div className="light">
-              <h1 className="cardtitle">FEATURED RESOURCES</h1>
-              <Row type="flex" justify="center" gutter={[32, 16]}>
-                {resourcesContent.data.featured.map(content => (
-                  <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-                    <Card className="involve-card">
-                      <h1 className="title">{content.title}</h1>
-                      <h3 className="source">{content.source}</h3>
-                      <p>{content.description}</p>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-              <h1 className="othertitle">OTHER RESOURCES</h1>
-              {resourcesContent.data.other.map(content => (
-                <a href={content.link} target="_blank">
-                  <div className="extra-data">
-                    <h1>{content.title}</h1>
-                    <h3>{content.source}</h3>
-                  </div>
-                </a>
-              ))}
-            </div>
-          </div>
-        </TabDiv>
-        <TabDiv label="All">
-          <div className="allcards">
-            <div className="tags">
-              <Tag className="tag" color="#FF9696">Process</Tag>
-              <Tag className="tag" color="#566ED1">Community</Tag>
-              <Tag className="tag" color="#B7DFE6">Data</Tag>
-            </div>
-
-            {resourcesContent.process.other.map(content => (
-              <a href={content.link} target="_blank">
-                <div className="extra">
-                  <h1>{content.title}</h1>
-                  <h3>{content.source}</h3>
-                </div>
-              </a>
-            ))}
-            {resourcesContent.community.other.map(content => (
-              <a href={content.link} target="_blank">
-                <div className="extra-community">
-                  <h1>{content.title}</h1>
-                  <h3>{content.source}</h3>
-                </div>
-              </a>
-            ))}
-            {resourcesContent.data.other.map(content => (
-              <a href={content.link} target="_blank">
-                <div className="extra-data">
-                  <h1>{content.title}</h1>
-                  <h3>{content.source}</h3>
-                </div>
-              </a>
-            ))}
-          </div>
-        </TabDiv>
-      </Tabs> */}
+      <div className="submitmore">
+        <p>
+          The D4SD community is actively generating resources. For a more
+          complete and up-to-date list of resources, please see the following{' '}
+          <a
+            href="https://docs.google.com/spreadsheets/d/1BuLEN-JLFrEEPW2mlQ-mk93tP4kpFaIHSOA1UZU1F-I/edit"
+            target="_blank"
+          >
+            Google Sheet
+          </a>
+          . We have also created a Google Form for you to submit resources
+          related to the{' '}
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfuHbBGqH3nonF2O04O-69y4J5CVcVEtUqCwG5suMaaEKGR2g/viewform"
+            target="_blank"
+          >
+            D4SD process or challenges
+          </a>
+        </p>
+      </div>
       <br />
       <br />
       <Footer />
